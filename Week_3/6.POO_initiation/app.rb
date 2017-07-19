746aacb2594152491f0a024b297524b29575c7b5
nanafuji <nana.fujiyama@orange.fr>
2017-07-19 18:03:18 +0200

    completed second part of lesson until orange tree

--- Week_3/6.POO_initiation/app.rb
+++ Week_3/6.POO_initiation/app.rb
@@ -1,16 +1,31 @@
 require 'pry'
 
 class User
-	attr_reader :firstname
-	attr_writer :lastname
-	
+#	attr_reader :firstname
+#	attr_writer :lastname
+	attr_accessor :firstname, :lastname
+	@@all = []
+
+#	def count
+#		return @@all
+#	end
+
 	def initialize (firstname, lastname)
 		@firstname = firstname
 		@lastname = lastname
-		puts "I'm a new user! My name is #{firstname} #{lastname}"
+#		@@all += 1
+		puts "Creating #{self.firstname}"
+		@@all.push(self)
+#		puts "I'm a new user! My name is #{firstname} #{lastname}"
 	end
 	def full_name
 		return "#{@firstname.capitalize} #{@lastname.capitalize}"
+	end
+#	def User.count
+#		return @@all
+#	end
+	def self.all
+		return @@all
 	end
 #	def set_name_to(some_string)
 #		@name = some_string
