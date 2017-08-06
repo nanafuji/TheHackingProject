19edae982ef35d13eca7c3f5a37678630e496645
nanafuji <nana.fujiyama@orange.fr>
2017-08-06 17:03:25 +0200

    finish Michael Hartl RoR book!

--- Week_4/1.Rails_tuto/sample_app/config/routes.rb
+++ Week_4/1.Rails_tuto/sample_app/config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get 'signup',	   to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   get '/login',		to: 'sessions#new'
   post '/login',	to: 'sessions#create'
@@ -21,5 +25,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
 end
