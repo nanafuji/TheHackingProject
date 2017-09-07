978c67a419237d01e7aa76944ca6320a24181501
nanafuji <nana.fujiyama@orange.fr>
2017-09-07 11:32:34 +0200

    test

--- Week_9/GoogleMapsAPI/bis-googlemaps/app/assets/javascripts/javascript.js
+++ Week_9/GoogleMapsAPI/bis-googlemaps/app/assets/javascripts/javascript.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var panda_house = {lat: 48.891493, lng: 2.345976};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: uluru
