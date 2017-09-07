a93b72c6edb9a633c415822c3f7cd20940405621
nanafuji <nana.fujiyama@orange.fr>
2017-09-07 11:24:44 +0200

    initial commit map implemented

--- Week_9/GoogleMapsAPI/googlemaps-exercise/app/assets/javascripts/javascript.js
+++ Week_9/GoogleMapsAPI/googlemaps-exercise/app/assets/javascripts/javascript.js
@@ -1,27 +1,25 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	function myMap() {
 		var mapProp = {
+			center:{lat:48.8915,lng:2.3459}
 			center: new google.maps.LatLng(48.891493, 2.345976),
-			zoom:10,
+			zoom:8
 		};
 		var map = new google.maps.Map(document.getElementById("googlemap"), mapProp);
 }
-/*		var newPlace = new google.maps.LatLng(48.852775,2.369336)
+		var newPlace = new google.maps.LatLng(48.852775,2.369336)
 		var marker = new google.maps.Marker({position:newPlace});
 		marker.setMap(map);
-		alert("hello!");*/
+		alert("hello!");
 
-/*$('#submit').val("salut");*/
+$('#submit').val("salut");
 
 $('#submit').on('click', function() {
 	alert('Form submitted');
 });
-
 });
 
-
-
-/*	function getLatLong() {
+	function getLatLong() {
 		alert("hello");
 		var latittud = getElementById('lat').value;
 		var longitud = getElementById('long').value;
@@ -32,3 +30,11 @@ $('#submit').on('click', function() {
 	getLatLong();
 );
 */
+
+function myMap() {
+	var mapProp = {
+		center:{lat:48.8915,lng:2.3459}
+		zoom:8,
+		};
+	var map = new google.maps.Map(document.getElementById("googlemap"), mapProp);
+}
